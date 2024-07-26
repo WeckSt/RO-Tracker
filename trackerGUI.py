88abf54eaed72d45a4e4fb30e7ad3f0766de5b33
TecZ <steven.weckwerth@gmail.com>
2024-07-26 10:29:20 +0200

    First creations, trying out UI design

--- trackerGUI.py
+++ trackerGUI.py
@@ -1,2 +1,14 @@
 from tkinter import *
 from tkinter import ttk
+
+root = Tk()
+root.title("roTracker")
+root.resizable(width=True, height=True)
+root.geometry("1400x600")
+
+my_menu = Menu(root)
+root.config(menu=my_menu)
+file_menu = Menu(my_menu, tearoff= False)
+my_menu.add_cascade(label="test",menu=file_menu)
+
+root.mainloop()
